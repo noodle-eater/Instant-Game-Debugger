d7fd86338ba66678ed754acc4cae1614b4522967
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-07 16:13:50 +0700

    Add HandleLog

--- Assets/GameDebugger/Scripts/GameDebugBehaviour.cs
+++ Assets/GameDebugger/Scripts/GameDebugBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public delegate void OnUpdateEvent();
 
@@ -7,10 +8,9 @@ public class GameDebugBehaviour : MonoBehaviour {
     
     public OnUpdateEvent OnUpdate;
 
-    public GameDebugBehaviour Instance { get; private set; }
     public List<TextUpdate> TextUpdates { get; private set; }
-    public GameObject DebugContent { get; private set; }
-    public GameObject ConsoleContent { get; private set; }
+    private GameObject DebugContent;
+    private GameObject ConsoleContent;
 
     private GameObject debugButton;
     private GameObject debugText;
@@ -18,37 +18,83 @@ public class GameDebugBehaviour : MonoBehaviour {
     private GameObject debugConsole;
     private GameObject consoleText;
 
-
-    private const string DEBUGCONTENTNAME = "Debug Content";
-    private const string CONSOLECONTENTNAME = "Console Content";
-    
+    private int counter = 0;    
 
     private void Awake() {
-        Instance = this;
         DebugContent = gameObject;
-        ConsoleContent = GameObject.Find(CONSOLECONTENTNAME);
         TextUpdates = new List<TextUpdate>();
+
+        debugButton = Resources.Load<GameObject>("Debug Button");
+        debugText = Resources.Load<GameObject>("Debug Text");
+        debugSeparator = Resources.Load<GameObject>("Debug Separator");
+        debugConsole = Resources.Load<GameObject>("Debug Console");
+        consoleText = Resources.Load<GameObject>("Console Text");
+
+        Application.logMessageReceived += HandleLog;
     }
 
     private void Update() {
-        
+        OnUpdate?.Invoke();
     }
 
     public GameObject InstantiateInCanvas(DebugType type) {
+        GameObject go = null;
         switch(type) {
-            case DebugType.Button : return Instantiate(debugButton);
-            case DebugType.Separator : return Instantiate(debugSeparator);
-            case DebugType.Text : return Instantiate(debugText);
-            case DebugType.Console : return Instantiate(debugConsole);
-            default: return null;
+            case DebugType.Button : 
+                go = Instantiate(debugButton);
+                break;
+            case DebugType.Separator : 
+                go = Instantiate(debugSeparator);
+                break;
+            case DebugType.Text : 
+                go = Instantiate(debugText);
+                break;
+            case DebugType.Console : 
+                go = Instantiate(debugConsole);
+                if(ConsoleContent == null) {
+                    ConsoleContent = GameObject.Find("Console Content");
+                }
+                break;
         }
+
+        if(go != null)
+            go.transform.SetParent(DebugContent.transform, false);
+        return go;
     }
 
     public void UpdateText() {
         foreach(var item in TextUpdates) {
             if(item.IsUpdate) {
-                item.Update();
+                item.TextValue.text = item.StrValue;
             }
+        }
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type) {
+        if (ConsoleContent == null)
+            return;
+        GameObject log = Instantiate(consoleText);
+        log.name = type + " - " + ++counter;
+        log.transform.SetParent(ConsoleContent.transform, false);
+        var LogText = log.GetComponent<Text>();
+
+        switch (type)
+        {
+            case LogType.Error:
+                LogText.text = "\t<color=red>" + type + " >> " + logString + "\n" + stackTrace + "</color>";
+                break;
+            case LogType.Exception:
+                LogText.text = "\t<color=red>" + type + " >> " + logString + "\n" + stackTrace + "</color>";
+                break;
+            case LogType.Warning:
+                LogText.text = "\t<color=yellow>" + type + " >> " + logString + "\n" + stackTrace + "</color>";
+                break;
+            case LogType.Log:
+                LogText.text = "\t" + type + " >> " + logString;
+                break;
+            case LogType.Assert:
+                LogText.text = "\t" + type + " >> " + logString;
+                break;
         }
     }
 
