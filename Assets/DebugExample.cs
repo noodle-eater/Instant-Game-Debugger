001db4c47bc7d28afafbf4d959a36e4f9724a418
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-07 16:33:23 +0700

    Add Debug Example

---
+++ Assets/DebugExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DebugExample : MonoBehaviour
+{
+
+    private int counter = 0;
+    
+    private void Start() {
+        var debug = new GameDebugger();
+        debug.Init();
+        debug.AddText("In Game Debugger");
+        debug.AddText(() => "Counter : " + counter);
+        debug.AddButton("Add", () => { counter++; Debug.Log("Counter " + counter); });
+        debug.AddConsole();
+    }
+}
