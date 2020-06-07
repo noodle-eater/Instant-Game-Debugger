30e58b3134747acd2921fb0b328bd121c9dce53a
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-07 16:37:26 +0700

    Add Trigger Error Example

--- Assets/GameDebugger/Example/DebugExample.cs
+++ Assets/GameDebugger/Example/DebugExample.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class DebugExample : MonoBehaviour
 {
@@ -7,10 +8,13 @@ public class DebugExample : MonoBehaviour
     
     private void Start() {
         var debug = new GameDebugger();
+        List<string> list = null;
+
         debug.Init();
         debug.AddText("In Game Debugger");
         debug.AddText(() => "Counter : " + counter);
         debug.AddButton("Add", () => { counter++; Debug.Log("Counter " + counter); });
+        debug.AddButton("Trigger Error", () => list.Add("Empty"));
         debug.AddConsole();
     }
 }
