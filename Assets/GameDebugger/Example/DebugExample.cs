7781c38b59311374a7f4ce34ec3f597e025689e0
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-09 21:16:29 +0700

    Remove unused script

--- Assets/GameDebugger/Example/DebugExample.cs
+++ Assets/GameDebugger/Example/DebugExample.cs
@@ -1,21 +1,16 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DebugExample : MonoBehaviour
 {
-
     private int counter = 0;
     
     private void Start() {
         var debug = new GameDebugger();
-        List<string> list = null;
 
         debug.Init();
         debug.AddText("In Game Debugger");
         debug.AddText(() => "Counter : " + counter);
         debug.AddButton("Add", () => { counter++; Debug.Log("Counter " + counter); });
-        debug.AddButton("Trigger Error", () => list.Add("Empty"));
-        // debug.AddButton("Copy Console Log", () => debug.CopyConsoleLog("Waw I can copy"));
         debug.AddConsole();
     }
 }
