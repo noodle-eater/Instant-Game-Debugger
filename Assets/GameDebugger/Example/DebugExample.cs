26dd087a9a31b4406189263876825afb3530f313
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-13 22:15:59 +0700

    Add Dropdown to example

--- Assets/GameDebugger/Example/DebugExample.cs
+++ Assets/GameDebugger/Example/DebugExample.cs
@@ -3,6 +3,7 @@
 public class DebugExample : MonoBehaviour
 {
     private int counter = 0;
+    private string[] foods = new string[] { "Apple", "Orange", "Noodle"};
     
     private void Start() {
         var debug = new GameDebugger();
@@ -11,6 +12,7 @@ public class DebugExample : MonoBehaviour
         debug.AddText("In Game Debugger");
         debug.AddText(() => "Counter : " + counter);
         debug.AddButton("Add", () => { counter++; Debug.Log("Counter " + counter); });
+        debug.AddDropDown(foods, (selectedIndex) => Debug.Log("I want to eat " + foods[selectedIndex]));
         debug.AddConsole();
     }
 }
