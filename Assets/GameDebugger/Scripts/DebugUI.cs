a59ab39a93fb141a5fe3c1fe36b9be24263fa270
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-13 22:15:11 +0700

    Move all debug ui to DebugUI script

---
+++ Assets/GameDebugger/Scripts/DebugUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DebugUI {
+    
+    public GameObject Button { get; private set; }
+    public GameObject Text { get; private set; }
+    public GameObject Separator { get; private set; }
+    public GameObject Dropdown { get; private set; }
+    public GameObject Console { get; private set; }
+    public GameObject ConsoleText { get; private set; }
+
+    public void Init() {
+        Button = Resources.Load<GameObject>("Debug Button");
+        Text = Resources.Load<GameObject>("Debug Text");
+        Separator = Resources.Load<GameObject>("Debug Separator");
+        Dropdown = Resources.Load<GameObject>("Debug Dropdown");
+        Console = Resources.Load<GameObject>("Debug Console");
+        ConsoleText = Resources.Load<GameObject>("Console Text");
+    }
+}
